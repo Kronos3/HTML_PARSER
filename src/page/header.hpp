2c9fc9fe957f46e681415ff308387474fe4138a0
Kronos3 <dovakhiin1359@gmail.com>
2016-04-17 22:41:54 +0000

    Addded the src dir, started backend

---
+++ src/page/header.hpp
@@ -0,0 +1,50 @@
+/*
+ * header.hpp
+ * 
+ * Copyright 2016 Andrei Tumbar <atuser@Kronos>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+ 
+#ifndef __HTML_PARSER_HEADER__
+#define __HTML_PARSER_HEADER__
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include "../tools/file.hpp"
+
+using namespace std;
+
+class header
+{
+    public:
+    
+    string source_file;
+    vector < string > &link_files;
+    vector < string > content;
+    
+    void init ( string _source_file, vector < string > &_link_files )
+    {
+        source_file = _source_file;
+        link_files = _link_files;
+        content = File ( _source_file ).readlines ( );
+    }
+};
+
+endif;
